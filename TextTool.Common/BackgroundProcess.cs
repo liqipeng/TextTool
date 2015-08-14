f74450c83ec75ab3b4b6c107abd581516fcb09ca
liqipeng <liqipeng127@gmail.com>
2015-08-14 18:05:48 +0800

    add BackgroundProcess
    Signed-off-by: liqipeng <liqipeng127@gmail.com>

---
+++ TextTool.Common/BackgroundProcess.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextTool.Common
+{
+    public abstract class BackgroundProcess<T> where T : TaskItem
+    {
+        protected bool IsStopped { get; set; }
+
+        public List<T> TaskItems { get; set; }
+
+        public BackgroundProcess()
+        {
+            this.IsStopped = true;
+        }
+
+        public void Start()
+        {
+            if (TaskItems == null)
+            {
+                throw new InvalidOperationException("TaskItems has not initialized.");
+            }
+
+            this.IsStopped = false;
+            Do();
+        }
+
+        public void Stop()
+        {
+            this.IsStopped = true;
+        }
+
+        protected virtual void Do() 
+        {
+        }
+
+        protected void AppendLog(string log)
+        {
+            if (LogOutputing != null)
+            {
+                LogOutputing(log);
+            }
+        }
+
+        protected void NotifyProgress(float progress)
+        {
+            if (OnProgressChanged != null)
+            {
+                OnProgressChanged(progress);
+            }
+        }
+
+        protected void Complete()
+        {
+            if (OnProgressChanged != null)
+            {
+                Completed();
+            }
+        }
+
+        public event Action<float> OnProgressChanged;
+        public event Action<string> LogOutputing;
+        public event Action Completed;
+    }
+}
