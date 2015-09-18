170f6c44c815b255215bc330d92d127cbe13fdb2
liqipeng <liqipeng127@gmail.com>
2015-09-18 21:17:51 +0800

    add GenerateXSDFromDll tool

---
+++ TextTool.GenerateXSDFromDll/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace TextTool.GenerateXSDFromDll
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.txtClassNames = new TextTool.Common.WindowsForm.EnhancedTextBox();
+            this.txtResult = new TextTool.Common.WindowsForm.EnhancedTextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtPath
+            // 
+            this.txtPath.Location = new System.Drawing.Point(78, 10);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.Size = new System.Drawing.Size(616, 21);
+            this.txtPath.TabIndex = 0;
+            // 
+            // btnOK
+            // 
+            this.btnOK.Location = new System.Drawing.Point(700, 8);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(75, 23);
+            this.btnOK.TabIndex = 1;
+            this.btnOK.Text = "OK";
+            this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // txtClassNames
+            // 
+            this.txtClassNames.Location = new System.Drawing.Point(15, 37);
+            this.txtClassNames.Multiline = true;
+            this.txtClassNames.Name = "txtClassNames";
+            this.txtClassNames.Size = new System.Drawing.Size(760, 114);
+            this.txtClassNames.TabIndex = 2;
+            // 
+            // txtResult
+            // 
+            this.txtResult.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.txtResult.Location = new System.Drawing.Point(0, 183);
+            this.txtResult.Multiline = true;
+            this.txtResult.Name = "txtResult";
+            this.txtResult.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtResult.Size = new System.Drawing.Size(800, 356);
+            this.txtResult.TabIndex = 3;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.txtPath);
+            this.panel1.Controls.Add(this.btnOK);
+            this.panel1.Controls.Add(this.txtClassNames);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 177);
+            this.panel1.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "dll路径：";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 158);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(119, 12);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "以英文逗号（,）分割";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 539);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.txtResult);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.Button btnOK;
+        private Common.WindowsForm.EnhancedTextBox txtClassNames;
+        private Common.WindowsForm.EnhancedTextBox txtResult;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ TextTool.GenerateXSDFromDll/Form1.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace TextTool.GenerateXSDFromDll
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            string dllPath = txtPath.Text.Trim();
+            string result = ReadEntityInfoFromDll(dllPath, this.txtClassNames.Text.Trim());
+            this.txtResult.Text = result;
+        }
+
+        private string ReadEntityInfoFromDll(string dllPath, string keywords)
+        {
+            StringBuilder sBuilder = new StringBuilder();
+
+            keywords.Split(',').ToList().ForEach((keyword) =>
+            {
+                try
+                {
+                    Assembly assembly = Assembly.LoadFrom(dllPath);
+                    Type type = assembly.GetTypes().Where(t => t.Name == keyword).Single();
+
+                    string xmlDocPath = Regex.Replace(new FileInfo(dllPath).FullName, "\\.[Dd][Ll]{2}$", ".XML");
+                    bool hasDoc = false;
+                    XDocument xDoc = new XDocument();
+
+                    if (File.Exists(xmlDocPath))
+                    {
+                        hasDoc = true;
+                        xDoc = XDocument.Load(xmlDocPath);
+                    }
+
+                    if (type.IsEnum)
+                    {
+                        sBuilder.AppendFormat(@"<xs:simpleType name=""{0}""><xs:restriction base=""xs:string"">", type.Name);
+
+                        if (hasDoc)
+                        {
+                            sBuilder.AppendFormat(@"<xs:annotation><xs:documentation>{0}</xs:documentation></xs:annotation>", GetComment(xDoc, CommentType.Type, type.Namespace + "." + type.Name));
+                        }
+
+                        Enum.GetNames(type).ToList().ForEach((name) =>
+                        {
+                            sBuilder.AppendFormat(@"<xs:enumeration value=""{0}""><xs:annotation>", name);
+
+                            if (hasDoc)
+                            {
+                                sBuilder.AppendFormat(@"<xs:annotation><xs:documentation>{0}</xs:documentation></xs:annotation>", GetComment(xDoc, CommentType.Property, type.Namespace + "." + type.Name + "." + name));
+                            }
+
+                            sBuilder.AppendFormat(@"<xs:appinfo>
+                                    <EnumerationValue xmlns=""http://schemas.microsoft.com/2003/10/Serialization/"">{1}</EnumerationValue>
+                                  </xs:appinfo>
+                                </xs:annotation>
+                              </xs:enumeration>", (int)Enum.Parse(type, name));
+                        });
+
+                        sBuilder.Append(@"</xs:restriction></xs:simpleType>");
+                    }
+                    else
+                    {
+                        sBuilder.AppendLine("<xs:complexType name=\"" + type.Name + "\"><xs:sequence>");
+
+                        if (hasDoc)
+                        {
+                            sBuilder.AppendFormat(@"<xs:annotation>
+			                    <xs:documentation>{0}</xs:documentation>
+		                    </xs:annotation>", GetComment(xDoc, CommentType.Type, type.Namespace + "." + type.Name));
+                        }
+
+                        type.GetProperties().ToList().ForEach((p) =>
+                        {
+                            sBuilder.AppendLine(string.Format("<xs:element name=\"{0}\" type=\"xs:{1}\">", p.Name, GetXSDTypeByCSharpType(p.PropertyType)));
+
+                            if (hasDoc)
+                            {
+                                sBuilder.AppendFormat(@"<xs:annotation>
+					                <xs:documentation>{0}</xs:documentation>
+				                </xs:annotation>", GetComment(xDoc, CommentType.Property, type.Namespace + "." + type.Name + "." + p.Name));
+                            }
+
+                            sBuilder.AppendLine("</xs:element>");
+                        });
+
+                        sBuilder.Append("</xs:sequence></xs:complexType>");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    sBuilder.AppendLine(ex.Message);
+                }
+            });
+
+            return sBuilder.ToString();
+        }
+
+        private string GetComment(XDocument xDoc, CommentType type, String name)
+        {
+            string t = "P";
+            if (type == CommentType.Type)
+            {
+                t = "T";
+            }
+
+            var summery = string.Empty;
+
+            try
+            {
+                summery = (from item in xDoc.Root.Element("members").Elements("member")
+                           where item.Attribute("name").Value == t + ":" + name
+                           select item.Element("summary").Value).First();
+            }
+            catch (Exception)
+            {
+            }
+
+            return summery;
+        }
+
+        private string GetXSDTypeByCSharpType(Type type)
+        {
+            Dictionary<Type, string> dict = new Dictionary<Type, string>() 
+            {
+                { typeof(byte), "byte" },
+                { typeof(decimal), "decimal" },
+                { typeof(int), "int" },
+                { typeof(long), "long" },
+                { typeof(short), "short" },
+                { typeof(string), "string" },
+                { typeof(DateTime), "dateTime" },
+                { typeof(bool), "boolean" },
+            };
+            if (dict.ContainsKey(type))
+            {
+                return dict[type];
+            }
+            else
+            {
+                return type.FullName;
+            }
+        }
+    }
+
+    enum CommentType
+    {
+        Type,
+        Property
+    }
+}
