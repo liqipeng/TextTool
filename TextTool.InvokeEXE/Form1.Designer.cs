85f63817a128d04aa6565718cb3376b450962e6b
liqipeng <liqipeng127@gmail.com>
2015-10-10 20:15:28 +0800

    new project 'Invoke EXE'

---
+++ TextTool.InvokeEXE/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace TextTool.InvokeEXE
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtPath = new TextTool.Common.WindowsForm.EnhancedTextBox();
+            this.startAndStopButton1 = new TextTool.Common.WindowsForm.StartAndStopButton();
+            this.txtLog = new TextTool.Common.WindowsForm.EnhancedTextBox();
+            this.SuspendLayout();
+            // 
+            // txtPath
+            // 
+            this.txtPath.Location = new System.Drawing.Point(12, 26);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.Size = new System.Drawing.Size(609, 21);
+            this.txtPath.TabIndex = 0;
+            // 
+            // startAndStopButton1
+            // 
+            this.startAndStopButton1.Location = new System.Drawing.Point(13, 54);
+            this.startAndStopButton1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.startAndStopButton1.Name = "startAndStopButton1";
+            this.startAndStopButton1.Size = new System.Drawing.Size(237, 24);
+            this.startAndStopButton1.TabIndex = 1;
+            this.startAndStopButton1.OnStartButtonClick += new System.EventHandler(this.startAndStopButton1_OnStartButtonClick);
+            this.startAndStopButton1.OnCancelButtonClick += new System.EventHandler(this.startAndStopButton1_OnCancelButtonClick);
+            // 
+            // txtLog
+            // 
+            this.txtLog.Location = new System.Drawing.Point(13, 92);
+            this.txtLog.Multiline = true;
+            this.txtLog.Name = "txtLog";
+            this.txtLog.Size = new System.Drawing.Size(634, 559);
+            this.txtLog.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(659, 663);
+            this.Controls.Add(this.startAndStopButton1);
+            this.Controls.Add(this.txtLog);
+            this.Controls.Add(this.txtPath);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Common.WindowsForm.EnhancedTextBox txtPath;
+        private Common.WindowsForm.StartAndStopButton startAndStopButton1;
+        private Common.WindowsForm.EnhancedTextBox txtLog;
+    }
+}
+
---
+++ TextTool.InvokeEXE/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TextTool.Common.WindowsForm;
+
+namespace TextTool.InvokeEXE
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void startAndStopButton1_OnStartButtonClick(object sender, EventArgs e)
+        {
+            string filename = "ping";
+            string arguments = "127.0.0.1";
+            bool recordLog = true;
+
+            Task.Factory.StartNew(() => {
+                Process proc = new Process();
+                proc.StartInfo.FileName = filename;
+                proc.StartInfo.CreateNoWindow = true;
+                proc.StartInfo.Arguments = arguments;
+                proc.StartInfo.RedirectStandardOutput = true;
+                proc.StartInfo.UseShellExecute = false;
+                proc.Start();
+
+                using (System.IO.StreamReader sr = new System.IO.StreamReader(proc.StandardOutput.BaseStream, Encoding.Default))
+                {
+                    string txt = sr.ReadToEnd();
+                    sr.Close();
+                    if (recordLog)
+                    {
+                        txtLog.SetTextSafe(txt);
+                    }
+                    if (!proc.HasExited)
+                    {
+                        proc.Kill();
+                    }
+                    return txt;
+                }
+            });
+        }
+
+        private void startAndStopButton1_OnCancelButtonClick(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
