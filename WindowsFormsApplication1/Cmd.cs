74565a8478f3a60474e558b6d613ae8532e5e955
liqipeng <liqipeng127@gmail.com>
2015-11-12 00:54:47 +0800

    从网上找的一个示例

---
+++ WindowsFormsApplication1/Cmd.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
+using System.Management;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication1
+{
+    public class Cmd
+    {
+        /// <summary>  
+        /// 是否终止调用CMD命令执行  
+        /// </summary>  
+        private static bool invokeCmdKilled = true;
+        /// <summary>  
+        /// 获取或设置是否终止调用CMD命令执行  
+        /// </summary>  
+        public static bool InvokeCmdKilled
+        {
+            get { return invokeCmdKilled; }
+            set
+            {
+                invokeCmdKilled = value;
+                if (invokeCmdKilled)
+                {
+                    if (p != null && !p.HasExited)
+                    {
+                        killProcess(p.Id);
+                    }
+                }
+            }
+        }
+        private static Process p;
+        private static Action<string> RefreshResult;
+
+        /// <summary>  
+        /// 调用CMD命令，执行指定的命令，并返回命令执行返回结果字符串  
+        /// </summary>  
+        /// <param name="cmdArgs">命令行</param>  
+        /// <param name="RefreshResult">刷新返回结果字符串的事件</param>  
+        /// <returns></returns>  
+        public static void InvokeCmd(string cmdArgs, Action<string> pRefreshResult = null)
+        {
+            InvokeCmdKilled = false;
+            RefreshResult = pRefreshResult;
+            if (p != null)
+            {
+                p.Close();
+                p = null;
+            }
+            p = new Process();
+            p.StartInfo.FileName = "cmd.exe";
+            p.ErrorDataReceived += new DataReceivedEventHandler(p_ErrorDataReceived);
+            p.OutputDataReceived += new DataReceivedEventHandler(p_OutputDataReceived);
+            p.StartInfo.UseShellExecute = false;
+            p.StartInfo.RedirectStandardInput = true;
+            p.StartInfo.RedirectStandardOutput = true;
+            p.StartInfo.RedirectStandardError = true;
+            p.StartInfo.CreateNoWindow = true;
+            p.Start();
+            p.BeginErrorReadLine();
+            p.BeginOutputReadLine();
+
+            string[] cmds = cmdArgs.Split(new char[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+            foreach (var v in cmds)
+            {
+                Thread.Sleep(200);
+                p.StandardInput.WriteLine(v);
+            }
+            //p.StandardInput.WriteLine("exit");  
+            p.WaitForExit();
+            p.Dispose();
+            p.Close();
+            p = null;
+            InvokeCmdKilled = true;
+        }
+        /// <summary>  
+        /// 输入交互式命令  
+        /// </summary>  
+        /// <param name="cmd"></param>  
+        public static void InputCmdLine(string cmd)
+        {
+            if (p == null) return;
+            string[] cmds = cmd.Split(new char[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+            foreach (var v in cmds)
+            {
+                Thread.Sleep(200);
+                p.StandardInput.WriteLine(v);
+            }
+        }
+        /// <summary>  
+        /// 异步读取标准输出信息  
+        /// </summary>  
+        /// <param name="sender"></param>  
+        /// <param name="e"></param>  
+        static void p_OutputDataReceived(object sender, DataReceivedEventArgs e)
+        {
+            if (RefreshResult != null && e.Data != null)
+                RefreshResult(e.Data + "\r\n");
+        }
+        /// <summary>  
+        /// 异步读取错误消息  
+        /// </summary>  
+        /// <param name="sender"></param>  
+        /// <param name="e"></param>  
+        static void p_ErrorDataReceived(object sender, DataReceivedEventArgs e)
+        {
+            if (RefreshResult != null && e.Data != null)
+            {
+                RefreshResult(e.Data + "\r\n");
+            }
+        }
+        /// <summary>  
+        /// 关闭指定进程ID的进程以及子进程（关闭进程树）  
+        /// </summary>  
+        /// <param name="id"></param>  
+        public static void FindAndKillProcess(int id)
+        {
+            killProcess(id);
+        }
+        /// <summary>  
+        /// 关闭指定进程名称的进程以及子进程（关闭进程树）  
+        /// </summary>  
+        /// <param name="name"></param>  
+        public static void FindAndKillProcess(string name)
+        {
+            foreach (Process clsProcess in Process.GetProcesses())
+            {
+                if ((clsProcess.ProcessName.StartsWith(name, StringComparison.CurrentCulture)) || (clsProcess.MainWindowTitle.StartsWith(name, StringComparison.CurrentCulture)))
+                    killProcess(clsProcess.Id);
+            }
+        }
+        /// <summary>  
+        /// 关闭进程树  
+        /// </summary>  
+        /// <param name="pid"></param>  
+        /// <returns></returns>  
+        private static bool killProcess(int pid)
+        {
+            Process[] procs = Process.GetProcesses();
+            for (int i = 0; i < procs.Length; i++)
+            {
+                if (getParentProcess(procs[i].Id) == pid)
+                    killProcess(procs[i].Id);
+            }
+
+            try
+            {
+                Process myProc = Process.GetProcessById(pid);
+                myProc.Kill();
+            }
+            //进程已经退出  
+            catch (ArgumentException)
+            {
+                ;
+            }
+            return true;
+        }
+        /// <summary>  
+        /// 获取父进程ID  
+        /// </summary>  
+        /// <param name="Id"></param>  
+        /// <returns></returns>  
+        private static int getParentProcess(int Id)
+        {
+            int parentPid = 0;
+            using (ManagementObject mo = new ManagementObject("win32_process.handle='" + Id.ToString(CultureInfo.InvariantCulture) + "'"))
+            {
+                try
+                {
+                    mo.Get();
+                }
+                catch (ManagementException)
+                {
+                    return -1;
+                }
+                parentPid = Convert.ToInt32(mo["ParentProcessId"], CultureInfo.InvariantCulture);
+            }
+            return parentPid;
+        }
+
+    }
+}
---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,290 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        #region Windows Form Designer generated code
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;  
+
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.AutoCompleteCustomSource.AddRange(new string[] {
+            "ASSOC",
+            "ATTRIB",
+            "BREAK",
+            "BCDEDIT",
+            "CACLS",
+            "CALL",
+            "CD",
+            "CHCP",
+            "CHDIR",
+            "CHKDSK",
+            "CHKNTFS",
+            "CLS",
+            "CMD",
+            "COLOR",
+            "COMP",
+            "COMPACT",
+            "CONVERT",
+            "COPY",
+            "DATE",
+            "DEL",
+            "DIR",
+            "DISKCOMP",
+            "DISKCOPY",
+            "DISKPART",
+            "DOSKEY",
+            "DRIVERQUERY",
+            "ECHO",
+            "ENDLOCAL",
+            "ERASE",
+            "EXIT",
+            "FC",
+            "FIND",
+            "FINDSTR",
+            "FOR",
+            "FORMAT",
+            "FSUTIL",
+            "FTYPE",
+            "GOTO",
+            "GPRESULT",
+            "GRAFTABL",
+            "HELP",
+            "ICACLS",
+            "IF",
+            "LABEL",
+            "MD",
+            "MKDIR",
+            "MKLINK",
+            "MODE",
+            "MORE",
+            "MOVE",
+            "OPENFILES",
+            "PATH",
+            "PAUSE",
+            "POPD",
+            "PRINT",
+            "PROMPT",
+            "PUSHD",
+            "RD",
+            "RECOVER",
+            "REM",
+            "REN",
+            "RENAME",
+            "REPLACE",
+            "RMDIR",
+            "ROBOCOPY",
+            "SET",
+            "SETLOCAL",
+            "SC",
+            "SCHTASKS",
+            "SHIFT",
+            "SHUTDOWN",
+            "SORT",
+            "START",
+            "SUBST",
+            "SYSTEMINFO",
+            "TASKLIST",
+            "TASKKILL",
+            "TIME",
+            "TITLE",
+            "TREE",
+            "TYPE",
+            "VER",
+            "VERIFY",
+            "VOL",
+            "XCOPY",
+            "WMIC"});
+            this.textBox1.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Append;
+            this.textBox1.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.CustomSource;
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.textBox1.Location = new System.Drawing.Point(7, 43);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(1117, 65);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "ping 10.84.184.1 -t";
+            this.textBox1.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.textBox1_PreviewKeyDown);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox1.Location = new System.Drawing.Point(7, 6);
+            this.richTextBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(1117, 489);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 18);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(217, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "请输入待执行的命令，回车执行";
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.button5);
+            this.splitContainer1.Panel1.Controls.Add(this.button4);
+            this.splitContainer1.Panel1.Controls.Add(this.button3);
+            this.splitContainer1.Panel1.Controls.Add(this.button2);
+            this.splitContainer1.Panel1.Controls.Add(this.button1);
+            this.splitContainer1.Panel1.Controls.Add(this.textBox1);
+            this.splitContainer1.Panel1.Controls.Add(this.label1);
+            this.splitContainer1.Panel1.Padding = new System.Windows.Forms.Padding(7, 0, 7, 6);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.richTextBox1);
+            this.splitContainer1.Panel2.Padding = new System.Windows.Forms.Padding(7, 6, 7, 6);
+            this.splitContainer1.Size = new System.Drawing.Size(1131, 620);
+            this.splitContainer1.SplitterDistance = 114;
+            this.splitContainer1.SplitterWidth = 5;
+            this.splitContainer1.TabIndex = 3;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(540, 11);
+            this.button5.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(125, 29);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "输入退出命令";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(427, 11);
+            this.button4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(100, 29);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "交互输入";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(308, 11);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(100, 29);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "开始执行";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(807, 11);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(127, 29);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "清空返回结果";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(681, 11);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 29);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "停止执行";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1131, 620);
+            this.Controls.Add(this.splitContainer1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Dos Command";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form2_FormClosing);
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form2_FormClosed);
+            this.Load += new System.EventHandler(this.Form2_Load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
